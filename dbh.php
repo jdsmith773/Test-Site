4d2d43b6369f82754408c4e38d81783f04bb3181
Josh <jdsmith773@gmail.com>
2017-05-23 06:12:18 -0400

    updated sign-up page
    Sign up page updated

---
+++ dbh.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "root", "just_janay");
+
+if (!$conn) {
+  die("Connection failed: ".mysqli_connect_error());
+}
+ ?>
---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php
+include 'dbh.php';
+
+$first = $_post['fisrt'];
+$last = $_post['last'];
+$uid = $_post['uid'];
+
+echo $first."<br>";
+echo $last."<br>";
+echo $uid."<br>";
+ ?>
