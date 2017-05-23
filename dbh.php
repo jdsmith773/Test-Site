8380e23d9810594852c899dce6b63266fb0a1f4e
Josh <jdsmith773@gmail.com>
2017-05-23 10:41:19 -0400

    update
    updated index.html to index.php.
    updated server info in dbh.php.
    updated contact and sign up page to add password

--- dbh.php
+++ dbh.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "just_janay");
+$conn = mysqli_connect("localhost", "JD", "JDjd;;12", "just_janay");
 
 if (!$conn) {
   die("Connection failed: ".mysqli_connect_error());
--- signup.php
+++ signup.php
@@ -1,11 +1,15 @@
 <?php
+
 include 'dbh.php';
 
-$first = $_post['fisrt'];
-$last = $_post['last'];
-$uid = $_post['uid'];
+$first = $_POST['fisrt'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd']
 
-echo $first."<br>";
-echo $last."<br>";
-echo $uid."<br>";
+$sql = "INSERT INTO user (first, last, uid, pwd)
+VALUES ('$first', '$last', '$uid', '$pwd')"
+$result =mysqli_query($conn, $sql);
+
+header("Location: index.php");
  ?>
